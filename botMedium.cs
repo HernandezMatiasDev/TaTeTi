78ba04e649d4217ce44445b7857a9eb12d840928
Matias Hernandez <hernandez.matias.dev@gmail.com>
2025-01-20 14:47:31 -0300

    Add files via upload

---
+++ botMedium.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TaTeTi_1._0
+{
+    public class BotMedium : Bot
+    {
+
+        public override byte[] playing(bool player)
+        {
+            
+            return new byte[] { 0, 0 }; // Retorna una jugada predecible
+        }
+    }
+}
